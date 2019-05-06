c295b96b21645be0a6ca54c5d98c8e14c8980886
erichdiniz <erichdiniz@hotmail.com>
2019-05-06 11:48:58 -0300

    Initial Commit

---
+++ Vai Chover iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vai Chover iOS
+//
+//  Created by Erich Diniz on 06/05/19.
+//  Copyright © 2019 Erich Diniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
